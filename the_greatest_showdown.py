143230857e030fc97830190516990bf778eac15d
b-smith94 <byrondsmith9@gmail.com>
2024-07-26 13:36:32 -0400

    added file to calculate the largest and smallest numbers in a set

--- the_greatest_showdown.py
+++ the_greatest_showdown.py
@@ -1,4 +1,17 @@
 x = int(input("What is the first number? "))
-y = int(input("What is the second number?"))
-z = int(input("What is the third number?"))
+y = int(input("What is the second number? "))
+z = int(input("What is the third number? "))
 
+if x >= y and x >= z:
+    print("The largest number is", x)
+elif y >= x and y >= z:
+    print("The largest number is", y)
+else:
+    print("The largest number is", z)
+
+if x <= y and x <= z:
+    print("The smallest number is", x)
+elif y <= x and y <= z:
+    print ("The smallest number is", y)
+else:
+    print ("The smallest number is", z)
